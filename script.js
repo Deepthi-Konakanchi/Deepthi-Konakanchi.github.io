465436faddc59e16c56e14f8fd74ee1d4031f110
Deepthi-Konakanchi <92187372+Deepthi-Konakanchi@users.noreply.github.com>
2026-04-21 19:27:16 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const chars = "ABCDEFGHJKMNPQRSTUVWXY3456789#%&*+";
+const targetText = "STUDIO";
+const displayElement = document.getElementById("scramble-text");
+
+let iteration = 0;
+
+const interval = setInterval(() => {
+    displayElement.innerText = targetText
+        .split("")
+        .map((letter, index) => {
+            if (index < iteration) {
+                return targetText[index];
+            }
+            return chars[Math.floor(Math.random() * chars.length)];
+        })
+        .join("");
+
+    if (iteration >= targetText.length) {
+        clearInterval(interval);
+        
+        // Hide loader after a short delay once text is finished
+        setTimeout(() => {
+            document.body.classList.add("loaded");
+        }, 500);
+    }
+
+    iteration += 1 / 3;
+}, 50);
